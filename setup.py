262c4d10d875bcf555f4f735921f7cde1d49befc
sanitaravel <alexander.koshcheev.msk@gmail.com>
2025-04-16 18:23:54 +0200

    Remove command-line argument parsing from setup script

--- setup.py
+++ setup.py
@@ -6,13 +6,6 @@ It uses the 'setup' module to perform the actual setup tasks.
 """
 
 from setup import run_setup
-import argparse
 
 if __name__ == "__main__":
-    # Parse command-line arguments
-    parser = argparse.ArgumentParser(description='Starship Analyzer Setup')
-    parser.add_argument('--debug', action='store_true', help='Enable debug output for verbose installation logs')
-    args = parser.parse_args()
-    
-    # Pass debug flag to the setup function
-    run_setup(debug=args.debug)
+    run_setup()
